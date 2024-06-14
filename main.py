28448a3c566e6613b473c1b699fc114c3faa92de
Igor Ferreira <igorferreirafonseca@gmail.com>
2024-06-14 16:10:47 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,10 @@
 from fastapi import FastAPI
 from database.base import Base
 from database.engine import engine
+from api.routes import router as vendedor_router
 
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
+
+app.include_router(vendedor_router)
